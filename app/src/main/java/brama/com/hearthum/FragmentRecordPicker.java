4a7c41f54b9db5062bc7dabcb45f9ab05d68d03c
Brama051 <vedran.abramovic@gmail.com>
2015-07-18 19:33:56 +0200

    Record picker

--- app/src/main/java/brama/com/hearthum/FragmentRecordPicker.java
+++ app/src/main/java/brama/com/hearthum/FragmentRecordPicker.java
@@ -3,5 +3,22 @@ package brama.com.hearthum;
 /**
  * Created by ABM on 18.07.2015..
  */
-public class FragmentRecordPicker {
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentRecordPicker extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        //Inflate the layout for this fragment
+
+        return inflater.inflate(
+                R.layout.fragment_record_picker, container, false);
+    }
 }
